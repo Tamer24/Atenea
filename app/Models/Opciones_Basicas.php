beeae8467dd8a4d00974e6619dd4509232dc60b2
Tamer24 <dionelibarrolas@gmail.com>
2022-08-25 10:49:45 -0300

    Models Creation

---
+++ app/Models/Opciones_Basicas.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Opciones_Basicas extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/Vinculo_No_Familiar.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vinculo_No_Familiar extends Model
+{
+    use HasFactory;
+}
