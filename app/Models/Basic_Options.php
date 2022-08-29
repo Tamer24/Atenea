383cb98d779f18731317129456aa2cf910be88de
Tamer24 <dionelibarrolas@gmail.com>
2022-08-29 09:39:44 -0300

    Model's Creation

---
+++ app/Models/Basic_Options.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Basic_Options extends Model
+{
+    use HasFactory;
+}
