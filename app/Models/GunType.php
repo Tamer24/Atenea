3852beb8878dd8ff02c114a9970fff70b8b0f713
Tamer24 <dionelibarrolas@gmail.com>
2022-08-29 10:14:21 -0300

    Models Created
    Models in english (good sh#t)

---
+++ app/Models/GunType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GunType extends Model
+{
+    use HasFactory;
+}
