ef1f7510e7910144d81927660b44e9092d8b4ef9
Tamer24 <dionelibarrolas@gmail.com>
2022-08-30 08:17:01 -0300

    Model second commit
    I changed the "_"(underscore) for no spaces because voyager won't work with underscore or another separator

--- app/Models/FormalEducationStage.php
+++ app/Models/FormalEducationStage.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class LocationData extends Model
+class FormalEducationStage extends Model
 {
     use HasFactory;
 }
