1ed246143e17a8e6f32a600fee7d751b123b1101
Tamer24 <dionelibarrolas@gmail.com>
2022-08-30 12:08:07 -0300

    Update Models
    models update to accomodate voyager managment of plural and singular names

--- app/Models/LocationDatum.php
+++ app/Models/LocationDatum.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Spatial;
 
 class LocationDatum extends Model
 {
     use HasFactory;
+    use Spatial;
+    protected $spatial = ['coordenadas'];
 }
---
+++ app/Models/TheftDegree.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+#comment ya se que significa licenciatura con dos e, pero voyager lo queria leer asi y bueno no me voy a pelear con algo superior a mi
+class TheftDegree extends Model
+{
+    use HasFactory;
+}
