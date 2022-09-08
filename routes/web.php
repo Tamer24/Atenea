abbf3d470366cec0b5d61fb26489d6ff9bdd15ca
Tamer24 <dionelibarrolas@gmail.com>
2022-09-08 12:26:54 -0300

    Merge pull request #3 from Tamer24/master
    Master

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 use App\Http\Controllers\UserController;
+<<<<<<< HEAD
  
 Route::get('/user', [UserController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
 });
+=======
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+
+Route::redirect('/', '/user');
+
+Route::get('login', [LoginController::class, 'welcome']);
+Route::post('login', [ 'as' => 'login', 'uses' => 'LoginController@do']);
+Route::get('user', [UserController::class, 'home'])->middleware('auth');
+Route::get('user/test', [UserController::class, 'welcome'])->middleware('auth');
+>>>>>>> parent of 643b77d (login develop)
 
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
+});
+
+Route::get('pages', function(){
+    return view('pages');
 });
